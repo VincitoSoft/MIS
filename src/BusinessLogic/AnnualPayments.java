f81d92e039760608e81a0985c17269e034e1417d
'Minudika' <'minudika001@gmail.com'>
2014-10-22 01:41:25 +0530

    Step 1: Following classes created: -ClassTimeTable -Guardian -Leave -Payment -Student -Teacher -Class

---
+++ src/BusinessLogic/AnnualPayments.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package BusinessLogic;
+
+/**
+ *
+ * @author WaRLoCk
+ */
+public class AnnualPayments {
+    private String studentIndex;
+    private boolean fee600_paid;
+    private boolean fee60_paid;
+    private String paidDate;
+
+    public AnnualPayments(){
+        fee600_paid=false;
+        fee60_paid=false;
+    }
+    /**
+     * @return the studentIndex
+     */
+    public String getStudentIndex() {
+        return studentIndex;
+    }
+
+    /**
+     * @param studentIndex the studentIndex to set
+     */
+    public void setStudentIndex(String studentIndex) {
+        this.studentIndex = studentIndex;
+    }
+
+    /**
+     * @return the fee600_paid
+     */
+    public boolean isFee600_paid() {
+        return fee600_paid;
+    }
+
+    /**
+     * @param fee600_paid the fee600_paid to set
+     */
+    public void setFee600_paid(boolean fee600_paid) {
+        this.fee600_paid = fee600_paid;
+    }
+
+    /**
+     * @return the fee60_paid
+     */
+    public boolean isFee60_paid() {
+        return fee60_paid;
+    }
+
+    /**
+     * @param fee60_paid the fee60_paid to set
+     */
+    public void setFee60_paid(boolean fee60_paid) {
+        this.fee60_paid = fee60_paid;
+    }
+    
+    
+    
+    
+}
+
