22d469cb54eea65b8628fd84203f383e088471c0
malaka <malakaganga.ac@gmail.com>
2014-10-22 02:04:02 +0530

    DataAccess Layer

--- src/DataAccess/ConnectToDB.java
+++ src/DataAccess/ConnectToDB.java
@@ -1,15 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package DataAccess;
-
+import java.sql.*;
 /**
  *
- * @author WaRLoCk
+ * @author Malaka
  */
 public class ConnectToDB {
-    
+
+   private static ConnectToDB singleton = new ConnectToDB( );
+   
+   /* A private Constructor prevents any other 
+    * class from instantiating.
+    */
+  private ConnectToDB(){
+  
+  };
+   
+   /* Static 'instance' method */ 
+   public static ConnectToDB getInstance( ) {//This return the singleton object instance
+      return singleton;
+   }
+   /* Other methods protected by singleton-ness */
+   /**
+    * 
+    * @return
+    */
+   public Statement accessDataBase( ) {
+       Statement statement=null;
+       
+       return statement;
+      
+   }
 }
